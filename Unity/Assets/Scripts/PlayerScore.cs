47ba7de8eff04ee5b8f66244871df08e52bcf6ff
Angrycranberie <mathdep38@sfr.fr>
2022-04-01 13:25:25 +0200

    Click upgrade ( + ???)
    Click upgrade fonctionnal. 
    Also 6000+ elements, maybe scrap or usefull tools ( > check later)
    edit : gitignore isn't set up

--- Unity/Assets/Scripts/PlayerScore.cs
+++ Unity/Assets/Scripts/PlayerScore.cs
@@ -5,18 +5,56 @@ namespace clicker
 {
     public class PlayerScore : MonoBehaviour
     {
-        [SerializeField] private TMP_Text _textbox;
+        [SerializeField] private TMP_Text _scoreDisplay;
+        [SerializeField] private TMP_Text _clickLevel;
+        [SerializeField] private TMP_Text _upgradeError;
+        [SerializeField] private TMP_Text _clickStatus;
+        private int _tentLevel = 0;
         private int _score;
+        private int _gain;
+        private int _cost = 20;
 
         private void OnEnable()
         {
-            _textbox.SetText(_score.ToString());
+            _scoreDisplay.SetText(_score.ToString());
+            _clickLevel.SetText("lvl." + _tentLevel.ToString());
+            _gain = (int)Mathf.Round(Mathf.Pow(2, _tentLevel));
+            _clickStatus.SetText(_gain.ToString()+" per click");
         }
 
         public void IncreaseScore()
         {
-            _score++;
-            _textbox.SetText(_score.ToString());
+            _score+= _gain;
+            _scoreDisplay.SetText(_score.ToString());
         }
+
+        public void LevelUp()
+        {
+            if (_score < 20)
+            {
+                _upgradeError.SetText("Missing ressources");
+                return;
+            }
+
+            
+            _score -= _cost;
+            
+            _tentLevel++;
+            
+            _gain = (int)Mathf.Round(Mathf.Pow(2, _tentLevel));
+
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            _upgradeError.SetText("");
+            _scoreDisplay.SetText(_score.ToString());
+            _clickLevel.SetText("lvl." + _tentLevel.ToString());
+            _clickStatus.SetText(_gain.ToString() + " per click");
+        }
+
+
+
     }
 }
